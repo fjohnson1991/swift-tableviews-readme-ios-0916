61cc8e99771b0fc24fd560a807aafa62fb4c6760
fjohnson1991 <felicity.johnson.mail@gmail.com>
2016-10-04 22:19:22 -0400

    commit

--- TableViewFunTime/TableViewController.swift
+++ TableViewFunTime/TableViewController.swift
@@ -57,6 +57,11 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
                 
         let cell = tableView.dequeueReusableCell(withIdentifier: "basicCell", for: indexPath)
+        
+        
+        
+        print("section: \(indexPath.section)")
+        print("row: \(indexPath.row)")
 
         let favoriteSong = favoriteSongs[(indexPath as NSIndexPath).row]
         
